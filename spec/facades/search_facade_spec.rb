f34881aab078312d6e629521519594beae2ebbcc
Kevin Cuadros <kev040798@gmail.com>
2021-01-17 18:11:37 -0500

    Get image method and test

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -23,4 +23,21 @@ describe SearchFacade do
     expect(facade.hourly_weather).to be_a Array
     expect(facade.hourly_weather.count).to eq(8)
   end
+
+  it 'returns a image object' do
+    location = 'denver,co'
+    json_response = File.read('spec/fixtures/image.json')
+    stub_request(:get, 'https://api.pexels.com/v1/search?query=denver,co')
+      .to_return(status: 200, body: json_response, headers: {})
+
+    search = SearchFacade.get_image(location)
+    expect(search).to be_a Image
+    expect(search.image).to be_a Hash
+    expected = %i[location image_url]
+    expect(search.image.keys).to eq(expected)
+    expect(search.credit).to be_a Hash
+    expected = %i[source author logo]
+    expect(search.credit.keys).to eq(expected)
+
+  end
 end
