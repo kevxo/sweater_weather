2f554f78d974285a943b57eb115fd3fa9b0d0b64
Kevin Cuadros <kev040798@gmail.com>
2021-01-18 20:16:07 -0500

    Create service and facade method for road trip

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -12,4 +12,15 @@ class SearchFacade
     data = ImageService.get_image(location)
     Image.new(data, location)
   end
+
+  def self.road_trip(from, to)
+    real_time = MapQuestService.get_estimated_time(from, to)
+    lon_lat = MapQuestService.get_lon_lat(to)
+    lat = lon_lat[:lat]
+    lon = lon_lat[:lng]
+
+    forcast = WeatherService.forcast(lat, lon)
+    weather = Forcast.new(forcast)
+    RoadTrip.new(from, to, real_time, weather)
+  end
 end
--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -9,6 +9,21 @@ class MapQuestService
     json[:results].last[:locations].last[:displayLatLng]
   end
 
+  def self.get_estimated_time(from, to)
+    response = conn.get('/directions/api/v2/routes') do |req|
+      req.params[:key] = ENV['KEY']
+      req.params[:from] = from
+      req.params[:to] = to
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    if json[:route][:realTime]
+      json[:route][:realTime]
+    else
+      json[:route]
+    end
+  end
+
   def self.conn
     Faraday.new(url: 'http://www.mapquestapi.com')
   end
